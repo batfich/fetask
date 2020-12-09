39d076cf75d87b4635554f1b3131c888d79a3562
batfich <nikolay.tachev@economedia.bg>
2020-12-09 06:11:38 +0200

    all elements without style

--- js/custom.js
+++ js/custom.js
@@ -7,8 +7,11 @@ $(document).ready(function () {
 		success: function (response) {
 			for (var i = 0; i < response.length; i++) {
 				countrry[response[i].name] = response[i];
-				dataCountry[response[i].name] = response[i].flag;
+				dataCountry[response[i].name] = response[i].null;
 			}
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log("Kole Poluchi li?");
 		},
 	});
 
@@ -18,6 +21,7 @@ $(document).ready(function () {
 		onAutocomplete: function (txt) {
 			countryInfo(countrry[txt]);
 			showWeader(countrry[txt]);
+			currencies(countrry[txt]);
 		},
 	});
 
@@ -25,8 +29,12 @@ $(document).ready(function () {
 		document.getElementById("name").innerHTML = data.name;
 		document.getElementById("capital").innerHTML = data.capital;
 		document.getElementById("region").innerHTML = data.region;
-		document.getElementById("timezones").innerHTML = data.timezones;
-		document.getElementById("lon").innerHTML = data.alpha2Code;
+		// document.getElementById("timezones").innerHTML = data.timezones;
+		// document.getElementById("alpha2Code").innerHTML = data.alpha2Code;
+		// document.getElementById("currencies").innerHTML = data.currencies[0].code;
+		data.timezones;
+		data.alpha2Code;
+		data.currencies[0].code;
 	}
 
 	function showWeader(data) {
@@ -36,15 +44,58 @@ $(document).ready(function () {
 			success: function (response) {
 				renderWeader(response);
 			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				console.log("Kole Poluchi li?");
+			},
 		});
 	}
 
 	function renderWeader(data) {
 		document.getElementById("weatherMain").innerHTML = data.weather[0].main;
 		document.getElementById("weatherDescription").innerHTML = data.weather[0].description;
-		document.getElementById("temp").innerHTML = Math.round(data.main.temp) + " °C";
-		document.getElementById("tempMin").innerHTML = Math.round(data.main.temp_min) + " °C Lo";
-		document.getElementById("tempMax").innerHTML = Math.round(data.main.temp_max) + " °C Max";
-		document.getElementById("humidity").innerHTML = data.main.humidity + " %";
+		document.getElementById("temp").innerHTML = Math.round(data.main.temp) + " <span>°C</span>";
+		document.getElementById("tempMin").innerHTML = Math.round(data.main.temp_min) + " <span>°C Lo</span>";
+		document.getElementById("tempMax").innerHTML = Math.round(data.main.temp_max) + " <span>°C Max</span>";
+		document.getElementById("humidity").innerHTML = data.main.humidity + " <span>%</span>";
 	}
+
+	function currencies(data) {
+		// $("#result").val("Loading...");
+		document.getElementById("amount-label").innerHTML = data.currencies[0].code;
+
+		var from = "USD";
+		var to = data.currencies[0].code;
+
+		$.ajax({
+			type: "GET",
+			url: "https://free.currconv.com/api/v7/convert?apiKey=df9f138a5090a29e4eaf&q=" + from + "_" + to + "&compact=y",
+			success: function (response) {
+				renderCurrencies(response);
+			},
+		});
+	}
+
+	function renderCurrencies(data) {
+		amount = document.getElementById("amount").value;
+		var exchangeRate = JSON.stringify(data).replace(/[^0-9\.]/g, "");
+		var result = amount * exchangeRate;
+
+		$("#result").val(parseFloat(result).toFixed(2));
+
+		$("#amount").keyup(function () {
+			amount = document.getElementById("amount").value;
+			var exchangeRate = JSON.stringify(data).replace(/[^0-9\.]/g, "");
+			var result = amount * exchangeRate;
+
+			$("#result").val(parseFloat(result).toFixed(2));
+		});
+	}
+
+	// var d = new Date();
+	// d + "";
+	// d.toUTCString();
+	// document.write(d);
 });
+
+// $("div.spanner").addClass("hide");
+// $("div.overlay").addClass("hide");
